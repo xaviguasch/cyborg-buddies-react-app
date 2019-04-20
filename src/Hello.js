243d4f8a923e8033a8c5d8e1ebac73b9bf9b1286
xaviguasch <xaviguasch@gmail.com>
2019-04-20 18:31:43 +0200

    added first component -testing-

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello there, fella</h1>
+        <p>Welcome to this app</p>
+      </div>
+    )
+  }
+}
+
+export default Hello
