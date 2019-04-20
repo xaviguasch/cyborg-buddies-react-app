b13d6050ec407e70ecd50021e464c84c0b4f6e5b
xaviguasch <xaviguasch@gmail.com>
2019-04-20 18:39:39 +0200

    added tachyons to style the css

--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react"
+import "./Hello.css"
 
 class Hello extends Component {
   render() {
     return (
-      <div>
+      <div class="f1 tc">
         <h1>Hello there, fella</h1>
         <p>Welcome to this app</p>
       </div>
