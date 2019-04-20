0a2c48e635367d4cd4a7155328e88fbb09551f8d
xaviguasch <xaviguasch@gmail.com>
2019-04-20 19:23:12 +0200

    added minimal styling to the Card component

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div>
+    <div className="bg-light-green dib br3 pa3 ma2 grow bwe shadow-5">
       <img src="https://robohash.org/test?200x200" alt="robots" />
       <div>
         <h2>Jane Doe</h2>
