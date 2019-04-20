6388c2f2e89699b2fde9ca3a2bea691b3b56ef29
xaviguasch <xaviguasch@gmail.com>
2019-04-20 19:08:13 +0200

    added Card component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import * as serviceWorker from "./serviceWorker"
-import "tachyons"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Card from './Card'
+import * as serviceWorker from './serviceWorker'
+import 'tachyons'
 
-ReactDOM.render(<h1>Hey man</h1>, document.getElementById("root"))
+ReactDOM.render(<Card />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
