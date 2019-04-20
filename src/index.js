f944ad3f12f16970a136d5e2e011d231e6f6cf98
xaviguasch <xaviguasch@gmail.com>
2019-04-20 18:55:32 +0200

    cleaned up app files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
-import Hello from "./Hello"
 import * as serviceWorker from "./serviceWorker"
 import "tachyons"
 
-ReactDOM.render(
-  <Hello greeting={"Hello, my man!"} />,
-  document.getElementById("root")
-)
+ReactDOM.render(<h1>Hey man</h1>, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
