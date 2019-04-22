0feeba48005751ca19b846b94767e1217bd12782
xaviguasch <xaviguasch@gmail.com>
2019-04-22 18:40:29 +0200

    added automatic robot filtering via search input updating the state

--- src/App.js
+++ src/App.js
@@ -11,12 +11,23 @@ class App extends Component {
       searchfield: ''
     }
   }
+
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value })
+  }
+
   render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase())
+    })
+
     return (
       <div className='tc'>
         <h1>CyborgBuddies</h1>
-        <SearchBox />
-        <CardList robots={robots} />
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     )
   }
