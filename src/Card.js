7d86f908d476f62f29def5818b1692d7bec64512
xaviguasch <xaviguasch@gmail.com>
2019-04-22 18:05:24 +0200

    refactored App component adding state

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow bwe shadow-5">
-      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bwe shadow-5'>
+      <img src={`https://robohash.org/${id}?200x200`} alt='robots' />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
