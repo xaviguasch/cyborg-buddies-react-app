41f6fe48d4a57779a9bb690e8571853888366842
xaviguasch <xaviguasch@gmail.com>
2019-04-22 20:18:13 +0200

    wrapped the CardList component inside an scrollable component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
+import Scroll from './Scroll'
 import './App.css'
 
 class App extends Component {
@@ -36,7 +37,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1'>CyborgBuddies</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filteredRobots} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
         </div>
       )
     }
