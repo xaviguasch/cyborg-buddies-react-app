5f66827162c556b30463a8dadf783ad93f709d8a
xaviguasch <xaviguasch@gmail.com>
2019-04-22 19:53:47 +0200

    fetched the cyborgs through an API instead of the robots.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import { robots } from './robots'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        return response.json()
+      })
+      .then(users => {
+        return this.setState({ robots: users })
+      })
   }
 
   onSearchChange = event => {
